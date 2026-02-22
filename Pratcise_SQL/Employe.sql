05c0b6995144db0d3d45cf53332cd98af72942b4
Aditya <adityarajsharma0700@gmail.com>
2026-02-22 13:12:29 +0530

    Updated SQl

--- Pratcise_SQL/Employe.sql
+++ Pratcise_SQL/Employe.sql
@@ -2,6 +2,6 @@ CREATE TABLE [dbo].[Employe]
 (
     [Id]         INT          NOT NULL PRIMARY KEY IDENTITY(1, 1),
     [Name]       NVARCHAR(20) NOT NULL,
-    [Salaries]   DECIMAL(7,5) NOT NULL,
+    [Salaries]   DECIMAL(7,2) NOT NULL,
     [Department] NVARCHAR(50) NOT NULL
 )
